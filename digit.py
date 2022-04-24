096ff3e79068d10196f54f370d4c27eebeef4d19
lzn <lzn@lzndeMacBook-Pro.local>
2022-04-23 22:17:45 -0400

    initial commit

---
+++ digit.py
@@ -0,0 +1,146 @@
+import argparse
+import os, sys
+import os.path as osp
+import torchvision
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import random, pdb, math, copy
+from tqdm import tqdm
+from scipy.spatial.distance import cdist
+import pickle
+from data_load import mnist, svhn, usps
+from torch.utils.data.dataset import Dataset
+import numpy as np
+
+
+class dataset_wrapper(Dataset):
+    def __init__(self, digit_dataset, seed=100, label_dir=None, data_frac=1.0, return_highdim=True):
+        assert(0 <= data_frac <= 1)
+        self.dataset = digit_dataset
+        self.return_highdim = return_highdim
+
+        n = len(self.dataset)
+        np.random.seed(seed)
+        self.indices = np.random.choice(np.arange(n), size=int(n * data_frac), replace=False)
+
+        if label_dir is not None:
+            self.mels = torch.from_numpy(np.load(label_dir))
+            self.highdim_label = True
+        else:
+            self.highdim_label = False
+
+    def __getitem__(self, index):
+        index = self.indices[index]
+        img, target = self.dataset.__getitem__(index)
+        if self.highdim_label and self.return_highdim:
+            return img, self.mels[target, ...]
+        return img, target
+
+    def __len__(self):
+        return len(self.indices)
+
+def digit_load(batch_size, dset, label_dir=None, data_frac=1.0): 
+    train_bs = batch_size
+    dset = dset
+    if dset == 's2m':
+        train_source = svhn.SVHN('./data/svhn/', split='train', download=True,
+                transform=transforms.Compose([
+                    transforms.Resize(32),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ]))
+        test_source = svhn.SVHN('./data/svhn/', split='test', download=True,
+                transform=transforms.Compose([
+                    transforms.Resize(32),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ]))  
+        train_target = mnist.MNIST('./data/mnist/', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.Resize(32),
+                    transforms.Lambda(lambda x: x.convert("RGB")),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ]))      
+        test_target = mnist.MNIST('./data/mnist/', train=False, download=True,
+                transform=transforms.Compose([
+                    transforms.Resize(32),
+                    transforms.Lambda(lambda x: x.convert("RGB")),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ]))
+    elif dset == 'u2m':
+        train_source = usps.USPS('./data/usps/', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.RandomCrop(28, padding=4),
+                    transforms.RandomRotation(10),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))
+        test_source = usps.USPS('./data/usps/', train=False, download=True,
+                transform=transforms.Compose([
+                    transforms.RandomCrop(28, padding=4),
+                    transforms.RandomRotation(10),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))    
+        train_target = mnist.MNIST('./data/mnist/', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))    
+        test_target = mnist.MNIST('./data/mnist/', train=False, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))
+    elif dset == 'm2u':
+        train_source = mnist.MNIST('./data/mnist/', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))
+        test_source = mnist.MNIST('./data/mnist/', train=False, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))
+
+        train_target = usps.USPS('./data/usps/', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))
+        test_target = usps.USPS('./data/usps/', train=False, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,))
+                ]))
+
+    train_source = dataset_wrapper(train_source, label_dir=label_dir, data_frac=data_frac)
+    test_source = dataset_wrapper(test_source, label_dir=label_dir)
+    train_target = dataset_wrapper(train_target, label_dir=label_dir)
+    test_target = dataset_wrapper(test_target, label_dir=label_dir, return_highdim=False)
+
+    datasets = {}
+    datasets["source_tr"] = train_source
+    datasets["source_te"] = test_source
+    datasets["target_tr"] = train_target
+    datasets["target_te"] = test_target
+
+    dset_loaders = {}
+    dset_loaders["source_tr"] = DataLoader(train_source, batch_size=train_bs, shuffle=True, 
+        num_workers=2, drop_last=False)
+    dset_loaders["source_te"] = DataLoader(test_source, batch_size=train_bs*2, shuffle=True, 
+        num_workers=2, drop_last=False)
+    dset_loaders["target"] = DataLoader(train_target, batch_size=train_bs, shuffle=True, 
+        num_workers=2, drop_last=False)
+    dset_loaders["target_te"] = DataLoader(train_target, batch_size=train_bs, shuffle=False, 
+        num_workers=2, drop_last=False)
+    dset_loaders["test"] = DataLoader(test_target, batch_size=train_bs*2, shuffle=False, 
+        num_workers=2, drop_last=False)
+    return dset_loaders, datasets
